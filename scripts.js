4d6ebfee037f69293310f2501f427e2a184ee13a
BrayanBertan <brayanbertan@gmail.com>
2020-09-15 21:49:05 -0300

    exercicio

---
+++ scripts.js
@@ -0,0 +1,19 @@
+var campoNome = document.getElementById("campoNome");
+var btnOk = document.getElementById("btnOk");
+var mensagemHW = document.getElementById("mensagem");
+btnOk.onclick = teste;
+var btnLimpar = document.getElementById("btnLimpar");
+btnLimpar.addEventListener("click",limpar);
+
+
+function teste() {
+    var nome = campoNome.value.trim();
+    var mensagem = "Hello Wold: " + nome;
+    mensagemHW.innerText = mensagem;
+    win
+}
+
+function limpar() {
+    campoNome.value = '';
+     mensagemHW.value = '';
+ }
